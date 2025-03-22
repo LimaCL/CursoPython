25e75f83a06aea5e9b092e0a3ab50226cfef4353
LimaCL <leandroc.lima23@hotmail.com>
2025-03-22 10:52:25 -0300

    Aulas
    Aulas 36 a 39

---
+++ Aulas/Logica de Programação/aula36.py
@@ -0,0 +1,14 @@
+# Operadores de atribuição
+# = | += | -= | *= | /= | //= | **= | %=
+
+contador = 1
+contador_string = 'A'
+contador_mult_string = 3
+#####
+
+contador += 2
+contador_string += 'B'
+contador_mult_string *= 'B'
+print(contador)
+print(contador_string)
+print(contador_mult_string)
---
+++ Aulas/aula39.py
@@ -0,0 +1,25 @@
+"""
+Iterando strings com while
+"""
+""" #       012345678910
+nome = 'Luiz Otávio'  # Iteráveis
+#      1110987654321
+tamanho_nome = len(nome)
+print(nome)
+print(tamanho_nome)
+print(nome[3])
+
+nova_string = ''
+nova_string += '*L*u*i*z* *O*t*á*v*i*o' """
+
+nome = 'Maria Helena'  # Iteráveis
+
+indice = 0
+novo_nome = ''
+while indice < len(nome):
+    letra = nome[indice]
+    novo_nome += f'*{letra}'
+    indice += 1
+
+novo_nome += '*'
+print(novo_nome)
